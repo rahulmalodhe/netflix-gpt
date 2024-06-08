17185c5fcd23c1ca5f187f9fede68676b03f8d86
Rahul Malodhe S <malodhe18@gmail.com>
2024-06-09 03:02:32 +0530

    changing keys values

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,7 +10,7 @@ export const OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: "Bearer" + import.meta.env.VITE_TMDB_KEY,
+    Authorization: "Bearer" + " "+ import.meta.env.VITE_TMDB_KEY,
   },
 };
 
--- src/utils/openai.js
+++ src/utils/openai.js
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 import {OPENAI_GPT_KEY} from "./constants";
-
+console.log( "OPENAI_GPT_KEY", OPENAI_GPT_KEY)
 const openai = new OpenAI({
   apiKey: OPENAI_GPT_KEY,
   dangerouslyAllowBrowser: true
